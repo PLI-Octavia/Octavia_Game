a4f77baf03088a02635867426050449fa353a11e
unknown <vasax.levy@gmail.com>
2016-09-07 16:53:03 +0200

    Le jeu qui rox

---
+++ New Unity Project/Assets/Script/Calcul.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Calcul {
+
+    public int x;
+    public int y;
+    public int operation;
+
+    public void generateCalcul()
+    {
+        System.Random rnd = new System.Random();
+       
+        this.x = rnd.Next(0,9);
+        this.y = rnd.Next(0, 9);
+        Debug.Log("X : " + x + "Y:" + y);
+    }
+}
---
+++ New Unity Project/Assets/Script/SpawnScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+    
+    public Calcul calcul;
+    public GameObject[] all_number;
+    // Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.calcul = new Calcul();
+            calcul.generateCalcul();
+
+            Instantiate(all_number[calcul.x], transform.position ,Quaternion.identity);
+            Instantiate(all_number[calcul.y], transform.position, Quaternion.identity);
+        }
+    }
+}
