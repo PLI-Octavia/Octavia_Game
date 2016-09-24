2e39942cd2f0331711a2e2de1e183c4c53703c80
unknown <vasax.levy@gmail.com>
2016-09-24 17:16:48 +0200

    gestion du Score

---
+++ New Unity Project/Assets/Script/ScoreManagement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using GlobalDatas;
+
+public class ScoreManagement : MonoBehaviour {
+
+    private int lastScore = 0;
+	// Use this for initialization
+	void Start () {
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (GameManagement.Score != lastScore)
+        {
+            Text tmp = GetComponent<Text>();
+            lastScore = GameManagement.Score;
+            tmp.text = "Score : " + GameManagement.Score;
+        }
+	}
+}
